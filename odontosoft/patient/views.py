1b7565c4d3468562f7933e9969465bd363093988
TheoRev <hrevillafernandez79@gmail.com>
2017-12-22 02:36:07 -0500

    Creando el frontend de Pacientes

--- odontosoft/patient/views.py
+++ odontosoft/patient/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
@@ -7,3 +7,8 @@ from .models import Patient
 
 def show_all_patients(request):
     patients = Patient.objects.order_by('id')
+    template = loader.get_template('patient/index.html')
+    context = {
+        'patients': patients
+    }
+    return HttpResponse(template.render(context, request))
